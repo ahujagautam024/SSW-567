e9a05fb64b3b54e52cbc91969addcb13eea1c829
ahujagautam024 <ahujagautam024@gmail.com>
2024-10-04 16:51:38 -0400

    Added Test cases

--- TestTriangle.py
+++ TestTriangle.py
@@ -23,8 +23,25 @@ class TestTriangles(unittest.TestCase):
     def testRightTriangleB(self): 
         self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
         
-    def testEquilateralTriangles(self): 
+    def testEquilateralTriangle(self): 
         self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+
+    def testScaleneTriangle(self): 
+        self.assertEqual(classifyTriangle(7, 8, 9), 'Scalene', '7,8,9 should be Scalene')
+
+    def testIsoscelesTriangleA(self): 
+        self.assertEqual(classifyTriangle(5, 5, 8), 'Isosceles', '5,5,8 should be Isosceles')
+
+    def testIsoscelesTriangleB(self): 
+        self.assertEqual(classifyTriangle(8, 5, 5), 'Isosceles', '8,5,5 should be Isosceles')
+
+    def testInvalidTriangleA(self): 
+        self.assertEqual(classifyTriangle(1, 10, 12), 'NotATriangle', '1,10,12 should be an invalid triangle')
+
+    def testInvalidTriangleB(self): 
+        self.assertEqual(classifyTriangle(0, 0, 0), 'InvalidInput', '0,0,0 should be an invalid triangle')
+
+
 
 if __name__ == '__main__':
     print('Running unit tests')
